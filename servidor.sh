9c94cd5acec99ee9551c05ab5215805cfa3ee396
poblete <ppoblete@talana.com>
2023-05-12 12:02:18 -0400

    fix menor de error en el log

--- servidor.sh
+++ servidor.sh
@@ -2,4 +2,3 @@ cd /home/ubuntu/proyecto/
 sudo docker-compose -f docker-compose-nginx.yml down
 sudo docker-compose -f docker-compose-nginx.yml pull
 sudo docker-compose -f docker-compose-nginx.yml up -d --build
-sudo docker-compose -f docker-compose-nginx.yml exec web python manage.py collectstatic
